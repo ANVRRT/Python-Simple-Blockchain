2729cedf6ffdf82289960d0e0239538462b2ceb5
ANVRRT <albertonavarreteramirez@gmail.com>
2021-08-14 01:59:40 -0500

    Modified Block.py

--- block.py
+++ block.py
@@ -1,37 +1,37 @@
 from hashlib import sha256
-from datetime import datetime
+from datetime import *
 
 class Block():
-    def __init__(self, data, previousBlockHash):
 
+    def __init__(self, data, previousBlockHash):
+        self.dateAndTime = datetime.now(timezone(timedelta(hours = -5)))
+        self.hash = ""
         self.data = data
         self.previousBlockHash = previousBlockHash
         self.compute_valid_hash()
 
-    def is_hash_valid(self,hash):
+    def is_hash_valid(self,possibleHash):
 
-        return (hash.startswith('0' * 4))
+        return (possibleHash.startswith('0' * 4))
     
     def convert_into_hash(self,preHash):
-
         encodedPreHash = preHash.encode()
         hash256 = sha256(encodedPreHash)
+        
         return (hash256.hexdigest())
         
 
     def compute_valid_hash(self):
-
-        hash = ''
         nonce = 0
 
-        while (not self.is_hash_valid(hash)):
+        while (not self.is_hash_valid(self.hash)):
             preHash = self.to_string() + str(nonce)
-            hash = self.convert_into_hash(preHash)
+            self.hash = self.convert_into_hash(preHash)
             nonce += 1
 
-        self.hash = hash
 
 
 
     def to_string(self):
-        return (f"{self.data}\t{self.previousBlockHash}")
+
+        return (f"{self.hash}\t{self.data}\t{self.dateAndTime}\t{self.previousBlockHash}\n")
