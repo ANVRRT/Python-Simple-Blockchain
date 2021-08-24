d4bd1f5cbd9b62714d03c4834ffe5f598fed280d
Alberto Navarrete <albertonavarreteramirez@gmail.com>
2021-08-24 15:36:41 -0500

    Added detailed comments

--- initialize_blockchain.py
+++ initialize_blockchain.py
@@ -3,47 +3,45 @@ from blockchain import Blockchain
 
 class BlockchainUtilities():
 
-    def __init__(self,blockchain) -> None:
+    def __init__(self,blockchain) -> None:                                          #Gets all the blocks of the blockchain sent as parameter.
         self.blocks = blockchain.get_blockchain()
 
     def show_blockchain(self) -> None:
-        bHashT = "Block Hash"
-        bDataT = "Block Data"
+        #<--------------- Starts defining format tags for the "table" showing blocks data -------------->
+        bHashT = "Block Hash" 
+        bDataT = "Block Data" 
         bDateTimeT = "Date&Time"
         bPrevHashT = "Block Previous Hash"
         sHash =((64 - len(bHashT))*" ")
         sDate = ((32 - len(bDateTimeT))* " ")
-        print(f"{bHashT} {sHash}\t{bDataT}\t{bDateTimeT}\t{sDate}\t{bPrevHashT}"  )
-
+        #<--------------- Ends defining format tags for the "table" showing blocks data -------------->
+        print(f"{bHashT} {sHash}\t{bDataT}\t{bDateTimeT}\t{sDate}\t{bPrevHashT}"  ) #Prints "table" header.
         for block in self.blocks:
-            print(block.to_string())
-
-
+            print(block.to_string())                                                #Prints each block data as a string.
 
 if __name__ == "__main__":
 
-    blockchain = Blockchain()
+    blockchain = Blockchain()                                   #Initializes blockchain
 
 
-    blockchain.add_new_block("Wallet 1")
+    blockchain.add_new_block("Wallet 1")                        #Adds new block
 
-    blockchain.add_new_block("Wallet 2")
+    blockchain.add_new_block("Wallet 2")                        #Adds new block
 
-    blockchain.add_new_block("Wallet 3")
+    blockchain.add_new_block("Wallet 3")                        #Adds new block
 
-    blockchain.add_new_block("Wallet 4")
+    blockchain.add_new_block("Wallet 4")                        #Adds new block
 
-    blockchain.add_new_block("Wallet 5")
+    blockchain.add_new_block("Wallet 5")                        #Adds new block
 
-    blockchain.add_new_block("Wallet 6")
+    blockchain.add_new_block("Wallet 6")                        #Adds new block
 
-    blockchain.add_new_block("Wallet 7")
+    blockchain.add_new_block("Wallet 7")                        #Adds new block
 
-    blockchain.add_new_block("Wallet 8")
-
-
-    utilities = BlockchainUtilities(blockchain)
-    utilities.show_blockchain()
+    blockchain.add_new_block("Wallet 8")                        #Adds new block
+    
+    blockchain.add_new_block("Wallet 8")                        #Adds new block
 
 
-
+    utilities = BlockchainUtilities(blockchain)                 #Initializes utilities for the sent blockchain
+    utilities.show_blockchain()                                 #Shows all data of blocks in the blockchain
